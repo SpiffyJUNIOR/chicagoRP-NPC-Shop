2988cbca7f17293f62b43d50cbedeb61ebbb252e
SpiffyJUNIOR <razer1546@gmail.com>
2023-02-05 00:10:16 -0800

    whoops

--- lua/chicagoRP_NPC_shop/sv_convars.lua
+++ lua/chicagoRP_NPC_shop/sv_convars.lua
@@ -1,6 +1,6 @@
-CreateConVar("sv_chicagoRP_NPCShop_enable", "1", {FCVAR_REPLICATED, FCVAR_NOTIFY}, "Enables or disables the NPC Shop.", 0, 1)
-CreateConVar("sv_chicagoRP_NPCShop_discounts", "1", {FCVAR_REPLICATED, FCVAR_NOTIFY}, "Enables or disables random discounts.", 0, 1)
-CreateConVar("sv_chicagoRP_NPCShop_discountchance", "1", {FCVAR_REPLICATED, FCVAR_NOTIFY}, "Changes the chance for a new discount, run every 60 seconds by default.", 1, 100)
-CreateConVar("sv_chicagoRP_NPCShop_discountdelay", "1", {FCVAR_REPLICATED, FCVAR_NOTIFY}, "In seconds, changes how often new discount creation is attempted.", 15)
+CreateConVar("sv_chicagoRP_NPCShop_enable", "1", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Enables or disables the NPC Shop.", 0, 1)
+CreateConVar("sv_chicagoRP_NPCShop_discounts", "1", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Enables or disables random discounts.", 0, 1)
+CreateConVar("sv_chicagoRP_NPCShop_discountchance", "1", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Changes the chance for a new discount, run every 60 seconds by default.", 1, 100)
+CreateConVar("sv_chicagoRP_NPCShop_discountdelay", "1", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "In seconds, changes how often new discount creation is attempted.", 15)
 
 print("chicagoRP NPC Shop server convars loaded!")
